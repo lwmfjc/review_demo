da19f78db7c70caaf16766dc69b43148c55e22b0
lwmfjc <lwmfjc@gmail.com>
2022-10-12 17:43:36 +0800

    测试

--- src/test/java/com/ly/demo/MyTest.java
+++ src/test/java/com/ly/demo/MyTest.java
@@ -1,6 +1,5 @@
 package com.ly.demo;
 
-import jdk.nashorn.internal.runtime.logging.Logger;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import sun.misc.Unsafe;
@@ -23,5 +22,22 @@ public class MyTest {
     @Test
     public void a(){
         log.info("hello,world!");
+        Unsafe unsafe = reflectGetUnsafe();
+        int size = 4;
+        long addr = unsafe.allocateMemory(size);
+        long addr3 = unsafe.reallocateMemory(addr, size * 2);
+        System.out.println("addr: "+addr);
+        System.out.println("addr3: "+addr3);
+        try {
+            unsafe.setMemory(null,addr ,size,(byte)1);
+            for (int i = 0; i < 2; i++) {
+                unsafe.copyMemory(null,addr,null,addr3+size*i,4);
+            }
+            System.out.println(unsafe.getInt(addr));
+            System.out.println(unsafe.getLong(addr3));
+        }finally {
+            unsafe.freeMemory(addr);
+            unsafe.freeMemory(addr3);
+        }
     }
 }
