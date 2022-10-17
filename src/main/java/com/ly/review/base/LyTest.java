fbe9b95effcac224c6b2f750f9065f766c8e7f99
lwmfjc <lwmfjc@gmail.com>
2022-10-17 17:38:36 +0800

    测试

--- src/main/java/com/ly/review/base/LyTest.java
+++ src/main/java/com/ly/review/base/LyTest.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.RandomAccess;
 
 @Slf4j
 public class LyTest {
@@ -19,6 +23,39 @@ public class LyTest {
     }
 
     public static void main(String[] args) {
+        //RandomAccess
+        //Collections
+        Comparable<String> a;
+        Comparator<String> b;
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(-1);
+        arrayList.add(3);
+        arrayList.add(3);
+        arrayList.add(-5);
+        arrayList.add(7);
+        arrayList.add(4);
+        arrayList.add(-9);
+        arrayList.add(-7);
+        System.out.println("原始数组:");
+        System.out.println(arrayList);
+        // void reverse(List list)：反转
+        Collections.reverse(arrayList);
+        System.out.println("Collections.reverse(arrayList):");
+        System.out.println(arrayList);
 
+        // void sort(List list),按自然排序的升序排序
+        Collections.sort(arrayList);
+        System.out.println("Collections.sort(arrayList):");
+        System.out.println(arrayList);
+        // 定制排序的用法
+        Collections.sort(arrayList, new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        System.out.println("定制排序后：");
+        System.out.println(arrayList);
     }
 }
